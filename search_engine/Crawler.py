1f0f5bfed69c28101532a0856251c46d4e9bfdd6
garudareiga <garudareiga@gmail.com>
2014-01-17 11:26:14 -0800

    Add requests and bs4

--- search_engine/Crawler.py
+++ search_engine/Crawler.py
@@ -6,28 +6,80 @@ Created on Jan 17, 2014
 
 import argparse
 import requests
+import Queue
+import logging
 from bs4 import BeautifulSoup
 
-class Crawler(object):
+web_crawl_logger = logging.getLogger(__name__)
+handler_stream = logging.StreamHandler()
+web_crawl_logger.addHandler(handler_stream)
+web_crawl_logger.setLevel(logging.DEBUG)
+
+class WebPage:
+    def __init__(self, url, depth):
+        self.url = url
+        self.depth = depth
+
+class Crawler:
     '''
     classdocs
     '''
 
-
-    def __init__(self, depth):
+    def __init__(self):
         '''
         Constructor
         '''
-        self.urls = []
+        self.depth = 0
         
-    def add_url(self, url):
-        self.urls.append(url)
+    def crawl(self, urls, max_depth):
+        web_page_queue = Queue.Queue()
+        for url in urls:
+            web_page = WebPage(url, 0)
+            web_page_queue.put(web_page)
         
-    def crawl(self):
+        while not web_page_queue.empty():
+            # parse_web_page
+            web_page = web_page_queue.get()
+            try:
+                r = requests.get(web_page.url)
+                #if r.status_code != requests.codes.OK:
+                #    raise
+                web_crawl_logger.info('Get {}'.format(web_page.url))
+                soup = BeautifulSoup(r.content)
+                self.parseText(soup)
+                
+                # Stop crawling on the current web page
+                if web_page.depth == max_depth:
+                    continue
+                # Continue crawling on the current web page
+                for a in soup.find_all('a'):
+                    next_web_page = WebPage(a.attrs['href'], web_page.depth + 1) 
+                    web_page_queue.put(next_web_page)      
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            #except Exception as e:
+            except:
+                web_crawl_logger.warn('Can *not* open url {}'.format(web_page.url))
+                continue
+            
+    
+    def parseText(self, soup):
         pass
     
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='A Simple Web Crawler')
+    parser.add_argument('--url', dest='urls', action='append',
+                       default=[], required=True,
+                       help='Add url to start crawl with',)
+    parser.add_argument('--depth', dest='depth', action='store',
+                       default=0, required=False,
+                       help='The link depth to crawl')
+    args = parser.parse_args()
+    
+    urls = args.urls
+    depth = args.depth
+    crawler = Crawler()
+    crawler.crawl(urls, depth)
         
 if __name__ == '__main__':
     main()
